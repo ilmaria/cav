1398b1245dde6452cba026e0c0edee2c53fab1ec
Ilmari Autio <ilmari.autio@gmail.com>
2017-11-15 23:43:52 +0200

    Something

--- build.bat
+++ build.bat
@@ -1,16 +1,17 @@
 @echo off
 
+set arch=x64
 set executable=cav
 set build_dir=build
 
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64  
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %arch%
 
 set debugflags=/Od /Zi
 set releaseflags=/Ox /GL /Qpar
 set devflags=/Zi
-set commonflags=/Wall /I "3rd_party\SDL2\include" /Fo%build_dir%\%executable%.obj
+set commonflags=/Wall /Fo%build_dir%\%executable%.obj /I "3rd_party\SDL2\include"
 
 mkdir %build_dir%
-set linkerflags=/OUT:%build_dir%\%executable%.exe
+set linkerflags=/out:%build_dir%\%executable%.exe /subsystem:console /libpath:3rd_party\SDL2\lib\%arch%
 
-cl.exe %releaseflags% %commonflags% %devflags% "src\main.c" /link %linkerflags%
+cl.exe %releaseflags% %commonflags% %devflags% "src\main.c" SDL2.lib SDL2main.lib /link %linkerflags%
--- src/main.c
+++ src/main.c
@@ -1,4 +1,45 @@
-int main(void)
+#pragma warning(push, 0)
+#include <SDL.h>
+#undef main
+#pragma warning(pop)
+#include <stdio.h>
+//Screen dimension constants
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+inline void log_error(const char *msg)
 {
+}
+
+int main(int argc, char *argv[])
+{
+    SDL_Window *window = NULL;
+    SDL_Surface *screenSurface = NULL;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "SDL could not initialize! %s\n", SDL_GetError());
+    }
+    else
+    {
+        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+        if (window == NULL)
+        {
+            SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Window could not be created! %s\n", SDL_GetError());
+        }
+        else
+        {
+            screenSurface = SDL_GetWindowSurface(window);
+            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+            SDL_UpdateWindowSurface(window);
+            SDL_Delay(2000);
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
