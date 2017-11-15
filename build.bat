b532bdcf6d2fb7705386d5574662b8d0400f68fb
Ilmari Autio <ilmari.autio@gmail.com>
2017-11-15 22:42:54 +0200

    Separate debug and release compiler flags

--- build.bat
+++ build.bat
@@ -5,13 +5,12 @@ set build_dir=build
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64  
 
-:: Debug
-::set compilerflags=/Od /Zi
-
-:: Release
-set compilerflags=/Ox /GL /Qpar /Wall /I "3rd_party\SDL2\include" /Fo%build_dir%\%executable%.obj
+set debugflags=/Od /Zi
+set releaseflags=/Ox /GL /Qpar
+set devflags=/Zi
+set commonflags=/Wall /I "3rd_party\SDL2\include" /Fo%build_dir%\%executable%.obj
 
 mkdir %build_dir%
 set linkerflags=/OUT:%build_dir%\%executable%.exe
 
-cl.exe %compilerflags% "src\main.c" /link %linkerflags%
+cl.exe %releaseflags% %commonflags% %devflags% "src\main.c" /link %linkerflags%
