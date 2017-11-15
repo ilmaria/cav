a601395c6a8a0bcbbd658a18e07151aa25316d6d
Ilmari Autio <ilmari.autio@gmail.com>
2017-11-15 22:33:16 +0200

    Add SDL

--- build.bat
+++ build.bat
@@ -1,13 +1,17 @@
 @echo off
 
+set executable=cav
+set build_dir=build
+
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64  
 
 :: Debug
 ::set compilerflags=/Od /Zi
 
 :: Release
-set compilerflags=/Ox /GL /Qpar /Wall
+set compilerflags=/Ox /GL /Qpar /Wall /I "3rd_party\SDL2\include" /Fo%build_dir%\%executable%.obj
 
-set linkerflags=/OUT:cav.exe
+mkdir %build_dir%
+set linkerflags=/OUT:%build_dir%\%executable%.exe
 
 cl.exe %compilerflags% "src\main.c" /link %linkerflags%
