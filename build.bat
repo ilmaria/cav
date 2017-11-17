da34e6520f7a47a1f8049e260b24bc53152e0c71
Ilmari Autio <ilmari.autio@gmail.com>
2017-11-17 11:42:14 +0200

    Separate dev and release builds for windows

--- build.bat
+++ build.bat
@@ -1,17 +1,34 @@
 @echo off
 
+set build_type=%1
 set arch=x64
 set executable=cav
-set build_dir=build
-
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %arch%
-
 set debugflags=/Od /Zi
 set releaseflags=/Ox /GL /Qpar
-set devflags=/Zi
-set commonflags=/Wall /Fo%build_dir%\%executable%.obj /I "3rd_party\SDL2\include"
+set devflags=%releaseflags% /Zi
+set commonflags=/Wall  /I "3rd_party\SDL2\include"
+set deps="3rd_party\SDL2\lib\x64\SDL2.dll"
+
+:: This enables cl.exe usage
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %arch%
+
+if "%build_type%"=="release" (
+    set build_dir=build\release
+    set compilerflags=%releaseflags% %commonflags% /Fo%build_dir%\%executable%.obj
+    set subsystem=/subsystem:windows
+) else (
+    set build_dir=build\dev
+    set compilerflags=%devflags% %commonflags% /Fo%build_dir%\%executable%.obj
+    set subsystem=/subsystem:console
+)
 
 mkdir %build_dir%
-set linkerflags=/out:%build_dir%\%executable%.exe /subsystem:console /libpath:3rd_party\SDL2\lib\%arch%
+set linkerflags=/out:%build_dir%\%executable%.exe %subsystem% /libpath:3rd_party\SDL2\lib\%arch%
+set libs=SDL2.lib SDL2main.lib
 
-cl.exe %releaseflags% %commonflags% %devflags% "src\main.c" SDL2.lib SDL2main.lib /link %linkerflags%
+cl.exe %compilerflags% "src\main.c" %libs% /link %linkerflags%
+
+:: Copy necessary dependencies into build dir
+for %%x in (%deps%) do (
+    copy %%x %build_dir%
+)
--- src/main.c
+++ src/main.c
@@ -1,6 +1,5 @@
 #pragma warning(push, 0)
 #include <SDL.h>
-#undef main
 #pragma warning(pop)
 #include <stdio.h>
 //Screen dimension constants
@@ -8,29 +7,28 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-inline void log_error(const char *msg)
+int main(int argc, char* argv[])
 {
-}
+    (void)argc;
+    (void)argv;
 
-int main(int argc, char *argv[])
-{
-    SDL_Window *window = NULL;
-    SDL_Surface *screenSurface = NULL;
+    SDL_Window* window = NULL;
+    SDL_Surface* screenSurface = NULL;
 
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "SDL could not initialize! %s\n", SDL_GetError());
-    }
-    else
-    {
-        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    } else {
+        window = SDL_CreateWindow(
+            "Game",
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            SCREEN_WIDTH,
+            SCREEN_HEIGHT,
+            SDL_WINDOW_SHOWN);
 
-        if (window == NULL)
-        {
+        if (window == NULL) {
             SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Window could not be created! %s\n", SDL_GetError());
-        }
-        else
-        {
+        } else {
             screenSurface = SDL_GetWindowSurface(window);
             SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
             SDL_UpdateWindowSurface(window);
