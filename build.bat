17e5aa2eea4dec1411a41be076832189f955a697
Ilmari Autio <ilmari.autio@gmail.com>
2017-11-19 22:34:09 +0200

    Fix windows build file

--- build.bat
+++ build.bat
@@ -1,34 +1,38 @@
 @echo off
 
 set build_type=%1
-set arch=x64
-set executable="cav"
+set out_dir=dist
+set app=cav
 set debugflags=/Od /Zi
 set releaseflags=/Ox /GL /Qpar
 set devflags=/Zi
-set commonflags=/Wall  /I "3rd_party\SDL2\include"
-set deps="3rd_party\SDL2\lib\x64\SDL2.dll"
+set commonflags=/Wall /I "include"
 
 :: This enables cl.exe usage
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %arch%
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 
 if "%build_type%"=="release" (
-    set build_dir=build\release
-    set compilerflags=%releaseflags% %commonflags% /Fo%build_dir%\%executable%.obj
+    set build_dir=%out_dir%\release
+    set compilerflags=%releaseflags% %commonflags% /Fo%build_dir%\%app%.obj /Fd%build_dir%\%app%.pdb
     set subsystem=/subsystem:windows
 ) else (
-    set build_dir=build\dev
-    set compilerflags=%devflags% %commonflags% /Fo%build_dir%\%executable%.obj
+    set build_dir=%out_dir%\dev
+    set compilerflags=%devflags% %commonflags% /Fo%build_dir%\%app%.obj /Fd%build_dir%\%app%.pdb
     set subsystem=/subsystem:console
 )
 
-mkdir %build_dir%
-set linkerflags=/out:%build_dir%\%executable%.exe %subsystem% /libpath:3rd_party\SDL2\lib\%arch%
-set libs=SDL2.lib SDL2main.lib
+if not exist %build_dir% mkdir %build_dir%
+
+set linkerflags=/out:%build_dir%\%app%.exe %subsystem% /libpath:lib
+set libs=^
+    SDL2\SDL2.lib^
+    SDL2\SDL2main.lib
 
 cl.exe %compilerflags% "src\main.c" %libs% /link %linkerflags%
 
 :: Copy necessary dependencies into build dir
+set deps="lib\SDL2\SDL2.dll"
+
 for %%x in (%deps%) do (
     copy %%x %build_dir%
 )
